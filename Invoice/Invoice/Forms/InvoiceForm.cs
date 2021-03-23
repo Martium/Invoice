aff8e2bae7e0ad14bbf5ffe8015775dea6f9efbb
Martynas Gedutis <martynasgedutis@gmail.com>
2021-03-23 21:33:13 +0200

    something

--- Invoice/Invoice/Forms/InvoiceForm.cs
+++ Invoice/Invoice/Forms/InvoiceForm.cs
@@ -8,13 +8,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Invoice
+namespace Invoice.Forms
 {
-    public partial class Form1 : Form
+    public partial class InvoiceForm : Form
     {
-        public Form1()
+        public InvoiceForm()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
         }
     }
 }
