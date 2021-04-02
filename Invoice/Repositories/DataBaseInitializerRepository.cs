250af15a64fa58e66f5389188becc969bf7ae61c
Martynas Gedutis <martynasgedutis@gmail.com>
2021-04-02 12:00:04 +0300

    create database folder

---
+++ Invoice/Repositories/DataBaseInitializerRepository.cs
@@ -0,0 +1,77 @@
+﻿using System.Data.SQLite;
+using System.IO;
+
+namespace Invoice.Repositories
+{
+    class DataBaseInitializerRepository
+    {
+        public void InitializeDatabaseIfNotExist()
+        {
+            if (File.Exists(AppConfiguration.DatabaseFile))
+            {
+#if DEBUG
+
+#else
+                return;
+#endif
+            }
+
+            if (!Directory.Exists(AppConfiguration.DatabaseFolder))
+            {
+                Directory.CreateDirectory(AppConfiguration.DatabaseFolder);
+            }
+            else
+            {
+                DeleteLeftoverFilesAndFolders();
+            }
+
+            SQLiteConnection.CreateFile(AppConfiguration.DatabaseFile);
+
+            using (var dbConnection = new SQLiteConnection(AppConfiguration.ConnectionString))
+            {
+                dbConnection.Open();
+
+                //CreateInvoiceTable(dbConnection);
+
+#if DEBUG
+               // FillInvoiceDefaultInfo(dbConnection);
+#endif
+
+            }
+        }
+
+        private void DeleteLeftoverFilesAndFolders()
+        {
+            var directory = new DirectoryInfo(AppConfiguration.DatabaseFolder);
+
+            foreach (FileInfo file in directory.GetFiles())
+            {
+                file.Delete();
+            }
+
+            foreach (DirectoryInfo subdirectory in directory.GetDirectories())
+            {
+                subdirectory.Delete(true);
+            }
+        }
+
+        private void CreateInvoiceFormTable(SQLiteConnection dbConnection)
+        {
+            string dropInvoiceTableQuery = GetDropTableQuery("Invoice");
+            SQLiteCommand dropAInvoiceTableCommand = new SQLiteCommand(dropInvoiceTableQuery, dbConnection);
+            dropAInvoiceTableCommand.ExecuteNonQuery();
+
+            string createInvoiceTableQuery =
+                @"
+                    CREATE TABLE [Invoice] (
+                        
+                    );
+                 ";
+        }
+
+        private string GetDropTableQuery(string tableName)
+        {
+            return $"DROP TABLE IF EXISTS [{tableName}]";
+        }
+    }
+}
