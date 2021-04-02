494f3443fa0e4de8a620b4118a7223d328a9c93c
Martynas Gedutis <martynasgedutis@gmail.com>
2021-04-02 12:17:48 +0300

    info buyer and seller lengths

---
+++ Invoice/Constants/TextBoxLengths.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invoice.Constants
+{
+    class TextBoxLengths
+    {
+        public static int SerialNumber => 10;
+        public static int SellerName => 800;
+        public static int SellerFirmCode => 800;
+        public static int SellerPvmCode => 800;
+        public static int SellerAddress => 800;
+        public static int SellerPhoneNumber => 800;
+        public static int SellerBank => 800;
+        public static int SellerBankAccountNumber => 800;
+        public static int SellerEmailAddress => 800;
+        public static int BuyerName => 800;
+        public static int BuyerFirmCode => 800;
+        public static int BuyerPvmCode => 800;
+        public static int BuyerAddress => 800;
+
+
+    }
+}
