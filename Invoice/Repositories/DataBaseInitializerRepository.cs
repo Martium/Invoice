ce0fb7c3499474a7b26772bca5ca6f2744f8747c
Martynas Gedutis <martynasgedutis@gmail.com>
2021-04-02 19:09:26 +0300

    sql fill error found

--- Invoice/Repositories/DataBaseInitializerRepository.cs
+++ Invoice/Repositories/DataBaseInitializerRepository.cs
@@ -36,7 +36,7 @@ namespace Invoice.Repositories
                 CreateInvoiceTable(dbConnection);
 
 #if DEBUG
-                // FillInvoiceTestingInfo(dbConnection);
+                FillInvoiceTestingInfo(dbConnection);
 #endif
 
             }
@@ -69,6 +69,7 @@ namespace Invoice.Repositories
                         [InvoiceNumber] [INTEGER] NOT NULL,
                         [InvoiceNumberYearCreation] [INTEGER] NOT NULL,
                         [InvoiceDate] [Date] NOT NULL,
+
                         [SerialNumber] [nvarchar]({FormSettings.TextBoxLengths.SerialNumber}) NULL,
                         [SellerName] [nvarchar]({FormSettings.TextBoxLengths.SellerName}) NULL,
                         [SellerFirmCode] [nvarchar]({FormSettings.TextBoxLengths.SellerFirmCode}) NULL,
@@ -78,10 +79,12 @@ namespace Invoice.Repositories
                         [SellerBank] [nvarchar]({FormSettings.TextBoxLengths.SellerBank}) NULL,
                         [SellerBankAccountNumber] [nvarchar]({FormSettings.TextBoxLengths.SellerBankAccountNumber}) NULL,
                         [SellerEmailAddress] [nvarchar]({FormSettings.TextBoxLengths.SellerEmailAddress}) NULL,
+
                         [BuyerName] [nvarchar]({FormSettings.TextBoxLengths.BuyerName}) NULL,
                         [BuyerFirmCode] [nvarchar]({FormSettings.TextBoxLengths.BuyerFirmCode}) NULL,
                         [BuyerPvmCode] [nvarchar]({FormSettings.TextBoxLengths.BuyerPvmCode}) NULL,
                         [BuyerAddress] [nvarchar]({FormSettings.TextBoxLengths.BuyerAddress}) NULL,
+
                         [FirstProductName] [nvarchar]({FormSettings.TextBoxLengths.FirstProductName}) NULL,
                         [SecondProductName] [nvarchar]({FormSettings.TextBoxLengths.SecondProductName}) NULL,
                         [ThirdProductName] [nvarchar]({FormSettings.TextBoxLengths.ThirdProductName}) NULL,
@@ -94,6 +97,7 @@ namespace Invoice.Repositories
                         [TenProductName] [nvarchar]({FormSettings.TextBoxLengths.TenProductName}) NULL,
                         [EleventhProductName] [nvarchar]({FormSettings.TextBoxLengths.EleventhProductName}) NULL,
                         [TwelfthProductName] [nvarchar]({FormSettings.TextBoxLengths.TwelfthProductName}) NULL,
+
                         [FirstProductSees] [nvarchar]({FormSettings.TextBoxLengths.FirstProductSees}) NULL,
                         [SecondProductSees] [nvarchar]({FormSettings.TextBoxLengths.SecondProductSees}) NULL,
                         [ThirdProductSees] [nvarchar]({FormSettings.TextBoxLengths.ThirdProductSees}) NULL,
@@ -106,6 +110,7 @@ namespace Invoice.Repositories
                         [TenProductSees] [nvarchar]({FormSettings.TextBoxLengths.TenProductSees}) NULL,
                         [EleventhProductSees] [nvarchar]({FormSettings.TextBoxLengths.EleventhProductSees}) NULL,
                         [TwelfthProductSees] [nvarchar]({FormSettings.TextBoxLengths.TwelfthProductSees}) NULL,
+
                         [FirstProductQuantity] [NUMERIC] NULL,
                         [SecondProductQuantity] [NUMERIC] NULL,
                         [ThirdProductQuantity] [NUMERIC] NULL,
@@ -118,6 +123,7 @@ namespace Invoice.Repositories
                         [TenProductQuantity] [NUMERIC] NULL,
                         [EleventhProductQuantity] [NUMERIC] NULL,
                         [TwelfthProductQuantity] [NUMERIC] NULL,
+
                         [FirstProductPrice] [NUMERIC] NULL,
                         [SecondProductPrice] [NUMERIC] NULL,
                         [ThirdProductPrice] [NUMERIC] NULL,
@@ -130,6 +136,7 @@ namespace Invoice.Repositories
                         [TenProductPrice] [NUMERIC] NULL,
                         [EleventhProductPrice] [NUMERIC] NULL,
                         [TwelfthProductPrice] [NUMERIC] NULL,
+
                         [PriceInWords] [nvarchar]({FormSettings.TextBoxLengths.PriceInWords}) NULL,
                         [InvoiceMaker] [nvarchar]({FormSettings.TextBoxLengths.InvoiceMaker}) NULL,
                         [InvoiceAccepted] [nvarchar]({FormSettings.TextBoxLengths.InvoiceAccepted}) NULL,
@@ -141,16 +148,19 @@ namespace Invoice.Repositories
             createInvoiceTableCommand.ExecuteNonQuery();
         }
 
-        private void FillInvoiceTestingInfo()
+        private void FillInvoiceTestingInfo(SQLiteConnection dbConnection)
         {
             string fillInvoiceTestingInfo =
                 $@"BEGIN TRANSACTION;
                     INSERT INTO 'Invoice'
                         Values (1, {DateTime.Now.Year}, '2021-04-02 10:09:03', 'ANA', 'Ežio ūkis', '305652600', 'LT 100013527916', 'Europos pr. 34-47, LT 46370 Kaunas', '867538581', 'Swedbank', 'LT857300010165352098', 
                         'ezioukis@gmail.com', 'Litbana', '110395066', 'LT100000022014', 'Kirtimų g. 57D, Vilnius', 'obuoliu sultys 3l', 'granatu sultys 3l', 'apelsinu sultys 3l', 'morku obuoliu mandarinu sultys 3l', 
-                        'morku obuoliu  sultys 3l', 'imbiero obuliu sultys 3l', 'obuliu kriausiu 3l', 'obuoliu vynuogiu 3l', ' obuoliu aronijų 3l', ' obuoliu juodujų serbentų sultys 3l', 'obuoliai 5l', 'obuoliu apelsinu 5l', 'vnt1', 'vnt2', 'vnt3', 'vnt4', 'vnt5', 'vnt6', 'vnt7', 'vnt8', 'vnt10', 'vnt11', 'vnt12', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1.1, 2.2, 2.3, 4.4, 5.5, 2.6, 2.7, 2.8, 2.9, 1, 1.11, 1.12, 'devyniasdešimtdevyni eurai 75ct', 'Direktorius Vitalijus Pranskūnas', 'Bazinga Bazingius is Libanos');
+                        'morku obuoliu  sultys 3l', 'imbiero obuliu sultys 3l', 'obuliu kriausiu 3l', 'obuoliu vynuogiu 3l', ' obuoliu aronijų 3l', ' obuoliu juodujų serbentų sultys 3l', 'obuoliai 5l', 'obuoliu apelsinu 5l', 'vnt1', 'vnt2', 'vnt3', 'vnt4', 'vnt5', 'vnt6', 'vnt7', 'vnt8', 'vnt9', 'vnt10', 'vnt11', 'vnt12', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1.1, 2.2, 2.3, 4.4, 5.5, 2.6, 2.7, 2.8, 2.9, 1, 1.11, 1.12, 'devyniasdešimtdevyni eurai 75ct', 'Direktorius Vitalijus Pranskūnas', 'Bazinga Bazingius is Libanos');
                    COMMIT;
                   ";
+
+            SQLiteCommand fillInvoiceTestingInfoTableCommand = new SQLiteCommand(fillInvoiceTestingInfo, dbConnection);
+            fillInvoiceTestingInfoTableCommand.ExecuteNonQuery();
         }
 
         private string GetDropTableQuery(string tableName)
