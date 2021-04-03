9a9412fa948bcf4e91c552ec886357d81bc5ce22
Martynas Gedutis <martynasgedutis@gmail.com>
2021-04-03 19:46:26 +0300

    FillDefaultSellerInfo class

---
+++ Invoice/Repositories/FillDefaultSellerInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace Invoice.Repositories
+{
+   public class FillDefaultSellerInfo
+   {
+       public string SellerName = "Ežio ūkis";
+       public string SellerFirmCode = "305652600";
+       public string SellerPvmCode = "LT100013527916";
+       public string SellerAddress = "Europos pr. 34-47, LT-46370 Kaunas";
+       public string SellerPhoneNumber = "8618222333";
+       public string SellerBank = "Swedbank";
+       public string SellerBankAccount = "LT857300010165352098";
+       public string SellerEmailAddress = "ezioukis@gmail.com";
+
+   }
+}
