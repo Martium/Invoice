a8c1df5123cae55a1fb66524614c2a41a978b241
Martynas Gedutis <martynasgedutis@gmail.com>
2021-04-04 02:32:13 +0300

    message box for  errors and information messages

---
+++ Invoice/Service/MessageDialogService.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+
+namespace Invoice.Service
+{
+    public class MessageDialogService
+    {
+        public static void ShowInfoMessage(string message)
+        {
+            MessageBox.Show(message, "Info Pranešimas", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        public static void ShowErrorMassage(string message)
+        {
+            MessageBox.Show(message, "Klaidos pranešimas", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
