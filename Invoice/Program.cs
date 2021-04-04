61218da3ddec84f630c7a077c30396f702ca9c1e
Martynas Gedutis <martynasgedutis@gmail.com>
2021-04-04 12:55:26 +0300

    uuid

--- Invoice/Program.cs
+++ Invoice/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Invoice.Forms;
@@ -10,6 +11,8 @@ namespace Invoice
 {
     static class Program
     {
+        private const string AppUuid = "caa556c2 - 952b-11eb-a8b3-0242ac130003";
+
         private static readonly DataBaseInitializerRepository DataBaseInitializerRepository = new DataBaseInitializerRepository();
         /// <summary>
         /// The main entry point for the application.
@@ -17,14 +20,23 @@ namespace Invoice
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-
-            bool isDataBaseInitialize = InitializeDatabase();
-
-            if (isDataBaseInitialize)
+            using (Mutex mutex = new Mutex(false, "Global\\" + AppUuid))
             {
-                Application.Run(new ListForm());
+                if (!mutex.WaitOne(0, false))
+                {
+                    MessageBox.Show(@"Vienu metu galima paleisti tik vieną 'Invoice' aplikacija!");
+                    return;
+                }
+
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+
+                bool isDataBaseInitialize = InitializeDatabase();
+
+                if (isDataBaseInitialize)
+                {
+                    Application.Run(new ListForm());
+                }
             }
         }
 
