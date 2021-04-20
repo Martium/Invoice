ea94a2c654f0a7c273e1a1dc3375871b99b71fa2
Martynas Gedutis <martynasgedutis@gmail.com>
2021-04-20 22:34:44 +0300

    seller info form

---
+++ Invoice/Forms/SellerInfoForm.Designer.cs
@@ -0,0 +1,279 @@
+﻿
+namespace Invoice.Forms
+{
+    partial class SellerInfoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SerialNumberLabel = new System.Windows.Forms.Label();
+            this.SellerNameLabel = new System.Windows.Forms.Label();
+            this.SellerFirmCodeLabel = new System.Windows.Forms.Label();
+            this.SellerPvmCodeLabel = new System.Windows.Forms.Label();
+            this.SellerAddressLabel = new System.Windows.Forms.Label();
+            this.BuyerPhoneNumberLabel = new System.Windows.Forms.Label();
+            this.SellerBankLabel = new System.Windows.Forms.Label();
+            this.SellerBankAccountNumberLabel = new System.Windows.Forms.Label();
+            this.SellerEmailAddressLabel = new System.Windows.Forms.Label();
+            this.SerialNumberRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.SellerNameRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.SellerFirmCodeRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.SellerPvmCodeRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.SellerAddressRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.SellerPhoneNumberRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.SellerBankRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.SellerBankAccountNumberRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.SellerEmailAddressRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SerialNumberLabel
+            // 
+            this.SerialNumberLabel.AutoSize = true;
+            this.SerialNumberLabel.Location = new System.Drawing.Point(12, 9);
+            this.SerialNumberLabel.Name = "SerialNumberLabel";
+            this.SerialNumberLabel.Size = new System.Drawing.Size(44, 13);
+            this.SerialNumberLabel.TabIndex = 2;
+            this.SerialNumberLabel.Text = "SERIJA";
+            // 
+            // SellerNameLabel
+            // 
+            this.SellerNameLabel.AutoSize = true;
+            this.SellerNameLabel.Location = new System.Drawing.Point(12, 60);
+            this.SellerNameLabel.Name = "SellerNameLabel";
+            this.SellerNameLabel.Size = new System.Drawing.Size(117, 13);
+            this.SellerNameLabel.TabIndex = 10;
+            this.SellerNameLabel.Text = "Pardavėjo pavadinimas";
+            // 
+            // SellerFirmCodeLabel
+            // 
+            this.SellerFirmCodeLabel.AutoSize = true;
+            this.SellerFirmCodeLabel.Location = new System.Drawing.Point(12, 95);
+            this.SellerFirmCodeLabel.Name = "SellerFirmCodeLabel";
+            this.SellerFirmCodeLabel.Size = new System.Drawing.Size(73, 13);
+            this.SellerFirmCodeLabel.TabIndex = 14;
+            this.SellerFirmCodeLabel.Text = "Įmonės kodas";
+            // 
+            // SellerPvmCodeLabel
+            // 
+            this.SellerPvmCodeLabel.AutoSize = true;
+            this.SellerPvmCodeLabel.Location = new System.Drawing.Point(12, 131);
+            this.SellerPvmCodeLabel.Name = "SellerPvmCodeLabel";
+            this.SellerPvmCodeLabel.Size = new System.Drawing.Size(62, 13);
+            this.SellerPvmCodeLabel.TabIndex = 18;
+            this.SellerPvmCodeLabel.Text = "PVM kodas";
+            // 
+            // SellerAddressLabel
+            // 
+            this.SellerAddressLabel.AutoSize = true;
+            this.SellerAddressLabel.Location = new System.Drawing.Point(12, 163);
+            this.SellerAddressLabel.Name = "SellerAddressLabel";
+            this.SellerAddressLabel.Size = new System.Drawing.Size(45, 13);
+            this.SellerAddressLabel.TabIndex = 22;
+            this.SellerAddressLabel.Text = "Adresas";
+            // 
+            // BuyerPhoneNumberLabel
+            // 
+            this.BuyerPhoneNumberLabel.AutoSize = true;
+            this.BuyerPhoneNumberLabel.Location = new System.Drawing.Point(12, 199);
+            this.BuyerPhoneNumberLabel.Name = "BuyerPhoneNumberLabel";
+            this.BuyerPhoneNumberLabel.Size = new System.Drawing.Size(88, 13);
+            this.BuyerPhoneNumberLabel.TabIndex = 26;
+            this.BuyerPhoneNumberLabel.Text = "Telefono numeris";
+            // 
+            // SellerBankLabel
+            // 
+            this.SellerBankLabel.AutoSize = true;
+            this.SellerBankLabel.Location = new System.Drawing.Point(14, 235);
+            this.SellerBankLabel.Name = "SellerBankLabel";
+            this.SellerBankLabel.Size = new System.Drawing.Size(43, 13);
+            this.SellerBankLabel.TabIndex = 28;
+            this.SellerBankLabel.Text = "Bankas";
+            // 
+            // SellerBankAccountNumberLabel
+            // 
+            this.SellerBankAccountNumberLabel.AutoSize = true;
+            this.SellerBankAccountNumberLabel.Location = new System.Drawing.Point(14, 272);
+            this.SellerBankAccountNumberLabel.Name = "SellerBankAccountNumberLabel";
+            this.SellerBankAccountNumberLabel.Size = new System.Drawing.Size(48, 13);
+            this.SellerBankAccountNumberLabel.TabIndex = 30;
+            this.SellerBankAccountNumberLabel.Text = "Sąskaita";
+            // 
+            // SellerEmailAddressLabel
+            // 
+            this.SellerEmailAddressLabel.AutoSize = true;
+            this.SellerEmailAddressLabel.Location = new System.Drawing.Point(14, 304);
+            this.SellerEmailAddressLabel.Name = "SellerEmailAddressLabel";
+            this.SellerEmailAddressLabel.Size = new System.Drawing.Size(53, 13);
+            this.SellerEmailAddressLabel.TabIndex = 32;
+            this.SellerEmailAddressLabel.Text = "El. paštas";
+            // 
+            // SerialNumberRichTextBox
+            // 
+            this.SerialNumberRichTextBox.BackColor = System.Drawing.SystemColors.Window;
+            this.SerialNumberRichTextBox.Location = new System.Drawing.Point(62, 6);
+            this.SerialNumberRichTextBox.Multiline = false;
+            this.SerialNumberRichTextBox.Name = "SerialNumberRichTextBox";
+            this.SerialNumberRichTextBox.Size = new System.Drawing.Size(83, 19);
+            this.SerialNumberRichTextBox.TabIndex = 33;
+            this.SerialNumberRichTextBox.Text = "";
+            // 
+            // SellerNameRichTextBox
+            // 
+            this.SellerNameRichTextBox.Location = new System.Drawing.Point(135, 60);
+            this.SellerNameRichTextBox.Multiline = false;
+            this.SellerNameRichTextBox.Name = "SellerNameRichTextBox";
+            this.SellerNameRichTextBox.Size = new System.Drawing.Size(205, 19);
+            this.SellerNameRichTextBox.TabIndex = 34;
+            this.SellerNameRichTextBox.Text = "";
+            // 
+            // SellerFirmCodeRichTextBox
+            // 
+            this.SellerFirmCodeRichTextBox.Location = new System.Drawing.Point(135, 92);
+            this.SellerFirmCodeRichTextBox.Multiline = false;
+            this.SellerFirmCodeRichTextBox.Name = "SellerFirmCodeRichTextBox";
+            this.SellerFirmCodeRichTextBox.Size = new System.Drawing.Size(205, 19);
+            this.SellerFirmCodeRichTextBox.TabIndex = 35;
+            this.SellerFirmCodeRichTextBox.Text = "";
+            // 
+            // SellerPvmCodeRichTextBox
+            // 
+            this.SellerPvmCodeRichTextBox.Location = new System.Drawing.Point(135, 125);
+            this.SellerPvmCodeRichTextBox.Multiline = false;
+            this.SellerPvmCodeRichTextBox.Name = "SellerPvmCodeRichTextBox";
+            this.SellerPvmCodeRichTextBox.Size = new System.Drawing.Size(205, 19);
+            this.SellerPvmCodeRichTextBox.TabIndex = 36;
+            this.SellerPvmCodeRichTextBox.Text = "";
+            // 
+            // SellerAddressRichTextBox
+            // 
+            this.SellerAddressRichTextBox.Location = new System.Drawing.Point(135, 157);
+            this.SellerAddressRichTextBox.Multiline = false;
+            this.SellerAddressRichTextBox.Name = "SellerAddressRichTextBox";
+            this.SellerAddressRichTextBox.Size = new System.Drawing.Size(205, 19);
+            this.SellerAddressRichTextBox.TabIndex = 37;
+            this.SellerAddressRichTextBox.Text = "";
+            // 
+            // SellerPhoneNumberRichTextBox
+            // 
+            this.SellerPhoneNumberRichTextBox.Location = new System.Drawing.Point(135, 193);
+            this.SellerPhoneNumberRichTextBox.Multiline = false;
+            this.SellerPhoneNumberRichTextBox.Name = "SellerPhoneNumberRichTextBox";
+            this.SellerPhoneNumberRichTextBox.Size = new System.Drawing.Size(205, 19);
+            this.SellerPhoneNumberRichTextBox.TabIndex = 38;
+            this.SellerPhoneNumberRichTextBox.Text = "";
+            // 
+            // SellerBankRichTextBox
+            // 
+            this.SellerBankRichTextBox.Location = new System.Drawing.Point(135, 229);
+            this.SellerBankRichTextBox.Multiline = false;
+            this.SellerBankRichTextBox.Name = "SellerBankRichTextBox";
+            this.SellerBankRichTextBox.Size = new System.Drawing.Size(205, 19);
+            this.SellerBankRichTextBox.TabIndex = 39;
+            this.SellerBankRichTextBox.Text = "";
+            // 
+            // SellerBankAccountNumberRichTextBox
+            // 
+            this.SellerBankAccountNumberRichTextBox.Location = new System.Drawing.Point(135, 269);
+            this.SellerBankAccountNumberRichTextBox.Multiline = false;
+            this.SellerBankAccountNumberRichTextBox.Name = "SellerBankAccountNumberRichTextBox";
+            this.SellerBankAccountNumberRichTextBox.Size = new System.Drawing.Size(205, 19);
+            this.SellerBankAccountNumberRichTextBox.TabIndex = 40;
+            this.SellerBankAccountNumberRichTextBox.Text = "";
+            // 
+            // SellerEmailAddressRichTextBox
+            // 
+            this.SellerEmailAddressRichTextBox.Location = new System.Drawing.Point(135, 304);
+            this.SellerEmailAddressRichTextBox.Multiline = false;
+            this.SellerEmailAddressRichTextBox.Name = "SellerEmailAddressRichTextBox";
+            this.SellerEmailAddressRichTextBox.Size = new System.Drawing.Size(205, 19);
+            this.SellerEmailAddressRichTextBox.TabIndex = 41;
+            this.SellerEmailAddressRichTextBox.Text = "";
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(135, 342);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(149, 23);
+            this.SaveButton.TabIndex = 42;
+            this.SaveButton.Text = "Išsaugoti";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            // 
+            // SellerInfoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 377);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.SellerEmailAddressRichTextBox);
+            this.Controls.Add(this.SellerBankAccountNumberRichTextBox);
+            this.Controls.Add(this.SellerBankRichTextBox);
+            this.Controls.Add(this.SellerPhoneNumberRichTextBox);
+            this.Controls.Add(this.SellerAddressRichTextBox);
+            this.Controls.Add(this.SellerPvmCodeRichTextBox);
+            this.Controls.Add(this.SellerFirmCodeRichTextBox);
+            this.Controls.Add(this.SellerNameRichTextBox);
+            this.Controls.Add(this.SerialNumberRichTextBox);
+            this.Controls.Add(this.SellerEmailAddressLabel);
+            this.Controls.Add(this.SellerBankAccountNumberLabel);
+            this.Controls.Add(this.SellerBankLabel);
+            this.Controls.Add(this.BuyerPhoneNumberLabel);
+            this.Controls.Add(this.SellerAddressLabel);
+            this.Controls.Add(this.SellerPvmCodeLabel);
+            this.Controls.Add(this.SellerFirmCodeLabel);
+            this.Controls.Add(this.SellerNameLabel);
+            this.Controls.Add(this.SerialNumberLabel);
+            this.MaximizeBox = false;
+            this.Name = "SellerInfoForm";
+            this.Text = "SellerInfoForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label SerialNumberLabel;
+        private System.Windows.Forms.Label SellerNameLabel;
+        private System.Windows.Forms.Label SellerFirmCodeLabel;
+        private System.Windows.Forms.Label SellerPvmCodeLabel;
+        private System.Windows.Forms.Label SellerAddressLabel;
+        private System.Windows.Forms.Label BuyerPhoneNumberLabel;
+        private System.Windows.Forms.Label SellerBankLabel;
+        private System.Windows.Forms.Label SellerBankAccountNumberLabel;
+        private System.Windows.Forms.Label SellerEmailAddressLabel;
+        private System.Windows.Forms.RichTextBox SerialNumberRichTextBox;
+        private System.Windows.Forms.RichTextBox SellerNameRichTextBox;
+        private System.Windows.Forms.RichTextBox SellerFirmCodeRichTextBox;
+        private System.Windows.Forms.RichTextBox SellerPvmCodeRichTextBox;
+        private System.Windows.Forms.RichTextBox SellerAddressRichTextBox;
+        private System.Windows.Forms.RichTextBox SellerPhoneNumberRichTextBox;
+        private System.Windows.Forms.RichTextBox SellerBankRichTextBox;
+        private System.Windows.Forms.RichTextBox SellerBankAccountNumberRichTextBox;
+        private System.Windows.Forms.RichTextBox SellerEmailAddressRichTextBox;
+        private System.Windows.Forms.Button SaveButton;
+    }
+}
