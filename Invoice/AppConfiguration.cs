267b93771338ceaa2f0a427475193de48a9023b3
Martynas Gedutis <martynasgedutis@gmail.com>
2021-08-10 23:02:17 +0300

    new database preparation

--- Invoice/AppConfiguration.cs
+++ Invoice/AppConfiguration.cs
@@ -6,10 +6,17 @@ namespace Invoice
     class AppConfiguration
     {
         private static readonly string DatabaseName = "InvoiceFactory";
+        private static readonly string DatabaseHelperName = "InvoiceHelpers";
         
         public static string PdfFolder => $"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}\\PdfFiles";
+
         public static string DatabaseFolder => $"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}\\Database";
+        public static string DatabaseFolderHelper => $"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}\\DatabaseHelper";
+
         public static string DatabaseFile => $"{DatabaseFolder}\\{DatabaseName}.db";
+        public static string DatabaseFileHelper => $"{DatabaseFolderHelper}\\{DatabaseHelperName}.db";
+
         public static string ConnectionString => $"Data Source={DatabaseFile};Version=3;UseUTF16Encoding=True;";
+        public static string ConnectionHelperString => $"Data Source={DatabaseFileHelper};Version=3;UseUTF16Encoding=True;";
     }
 }
